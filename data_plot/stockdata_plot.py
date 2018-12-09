d2f23acd8308b4fa8b7880d92e870e243f731353
Kaz Wong <kaz_wong@sae.com.hk>
2018-12-09 18:01:31 +0800

    new param for plot

--- data_plot/stockdata_plot.py
+++ data_plot/stockdata_plot.py
@@ -7,4 +7,18 @@ returns.plot()
 plt.title('Stock Data')
 plt.xlabel('Time')
 plt.legend(loc=0)
+
+prices = pd.read_excel('stock_data.xlsx', sheet_name=2, index_col=0, usecols="A,B,C,D,E")
+prices.plot()
+plt.title('Prices')
+plt.xlabel('Time')
+plt.legend(loc=0)
+
+bb = pd.read_excel('stock_data.xlsx', sheet_name=2, index_col=0, usecols="A,B,E,M,N,O")
+bb.plot()
+plt.title('BB')
+plt.xlabel('Time')
+plt.legend(loc=0)
+
+
 plt.show();
