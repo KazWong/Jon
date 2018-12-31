8e029d2329512e54638d8433330dd2599474ea96
Kaz Wong <kaz_wong@sae.com.hk>
2018-12-31 14:32:35 +0800

    multi plot

--- data_plot/stockdata_plot.py
+++ data_plot/stockdata_plot.py
@@ -1,24 +1,38 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.ticker as mticker
+from mpl_finance import candlestick_ohlc
 
 returns = pd.read_excel('stock_data.xlsx', sheet_name=2, index_col=0)
-returns.plot()
+
+
+plt.figure(1)
+
+print(returns['PX_OPEN'])
+
+plt.subplot(311)
+#returns.plot()
 plt.title('Stock Data')
 plt.xlabel('Time')
 plt.legend(loc=0)
+plt.plot(returns)
 
+plt.subplot(312)
 prices = pd.read_excel('stock_data.xlsx', sheet_name=2, index_col=0, usecols="A,B,C,D,E")
-prices.plot()
+#prices.plot()
 plt.title('Prices')
 plt.xlabel('Time')
 plt.legend(loc=0)
+plt.plot(prices)
 
+
+plt.subplot(313)
 bb = pd.read_excel('stock_data.xlsx', sheet_name=2, index_col=0, usecols="A,B,E,M,N,O")
-bb.plot()
+#bb.plot()
 plt.title('BB')
 plt.xlabel('Time')
 plt.legend(loc=0)
-
+plt.plot(bb)
 
 plt.show();
