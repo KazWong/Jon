0173da295747337c265b6e898ca57d2cc58e9f7c
Kaz Wong <kaz_wong@sae.com.hk>
2019-01-12 12:25:26 +0800

    plot candystick

--- data_plot/stockdata_plot.py
+++ data_plot/stockdata_plot.py
@@ -1,24 +1,29 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import matplotlib.ticker as mticker
-from mpl_finance import candlestick_ohlc
+import matplotlib.ticker as ticker
+import matplotlib.dates as mdates
+import datetime as datetime
+from mpl_finance import candlestick2_ohlc
 
 returns = pd.read_excel('stock_data.xlsx', sheet_name=2, index_col=0)
+date = pd.read_excel('stock_data.xlsx', sheet_name=2)
 
 
 plt.figure(1)
 
-print(returns['PX_OPEN'])
+print(date)
 
-plt.subplot(311)
+'''
+plt.subplot(411)
 #returns.plot()
 plt.title('Stock Data')
 plt.xlabel('Time')
 plt.legend(loc=0)
 plt.plot(returns)
-
-plt.subplot(312)
+'''
+'''
+plt.subplot(311)
 prices = pd.read_excel('stock_data.xlsx', sheet_name=2, index_col=0, usecols="A,B,C,D,E")
 #prices.plot()
 plt.title('Prices')
@@ -26,13 +31,29 @@ plt.xlabel('Time')
 plt.legend(loc=0)
 plt.plot(prices)
 
-
-plt.subplot(313)
+plt.subplot(312)
 bb = pd.read_excel('stock_data.xlsx', sheet_name=2, index_col=0, usecols="A,B,E,M,N,O")
 #bb.plot()
 plt.title('BB')
 plt.xlabel('Time')
 plt.legend(loc=0)
 plt.plot(bb)
+'''
 
-plt.show();
+ax = plt.subplot()
+candlestick2_ohlc(ax, returns['PX_OPEN'], returns['PX_HIGH'], returns['PX_LOW'], returns['PX_LAST'], width=0.3)
+xdate = returns.index
+
+#ax.xaxis.set_major_locator(ticker.MaxNLocator(6))
+
+def mydate(x, pos) :
+  try:
+    return xdate[int(x)]
+  except IndexError:
+    return ''
+    
+ax.xaxis.set_major_formatter(ticker.FuncFormatter(mydate))
+
+
+plt.tight_layout()
+plt.show()
